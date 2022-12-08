8c0536c6151acad5455688c8a3e8fee13db5ca9c
Momihe <momihe@mail.ru>
2022-12-08 16:47:40 +0700

    C#_seminar_2_hw

---
+++ Ex10/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное 
+число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.Clear();
+
+Console.Write("Введите трехзначное число: ");
+
+int n = (Convert.ToInt32(Console.ReadLine()));
+
+while (Math.Abs(n) < 100 || Math.Abs(n) > 999)
+{
+    Console.Write("Неверно. Введите трехзначное число: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+int a = n;
+
+Console.WriteLine($"Вторая цифра числа: {a}, равна: {Math.Abs((n / 10) % 10)}");
---
+++ Ex_Bonus/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задана последовательность натуральных чисел, завершающаяся числом 0. 
+Требуется определить значение второго по величине элемента в этой последовательности,
+то есть элемента, который будет наибольшим, 
+если из последовательности удалить наибольший элемент.
+*/
+
+Console.Clear();
+Console.WriteLine("Введите число или введите «0» для конца цикла.");
+int n = Convert.ToInt32(Console.ReadLine());
+int max = -1;
+int smax = -2;
+int temp = 0;
+while (n != 0)
+{ 
+    n = Convert.ToInt32(Console.ReadLine());
+    if (smax < n) smax = n;
+    if (smax > max)
+        {
+            temp = max;
+            max = smax;
+            smax = temp;  
+        }  
+}
+Console.WriteLine ($"Второе максимальное число = {smax}");
+Console.WriteLine("Конец");
